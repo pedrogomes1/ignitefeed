6652bdb90d7488a4a83f5d5cb6d5d4f93fcef207
Pedro <pedro-gomes17@hotmail.com>
2022-06-02 22:12:29 -0300

    feat: create Post component

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,57 @@
+import { Comment } from "../Comment";
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/pedrogomes1.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Pedro Gomes</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 23:00h" dateTime="02-02-2022 08:00:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
