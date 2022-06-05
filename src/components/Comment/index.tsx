a7e1aeeaa5ac9d326088e319c11adce1c2b46aa2
Pedro <pedro-gomes17@hotmail.com>
2022-06-05 12:38:15 -0300

    chore: added comment like functionality

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "../Avatar";
 
@@ -9,8 +10,14 @@ type CommentProps = {
 };
 
 export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((prev) => prev + 1);
   }
 
   return (
@@ -36,9 +43,9 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
