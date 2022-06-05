5df9df3a8d66f4045822b1cbd465462b05489b17
Pedro <pedro-gomes17@hotmail.com>
2022-06-05 11:55:11 -0300

    chore: render a dynamic comment

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -3,7 +3,11 @@ import { Avatar } from "../Avatar";
 
 import styles from "./Comment.module.css";
 
-export function Comment() {
+type CommentProps = {
+  content: string;
+};
+
+export function Comment({ content }: CommentProps) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/pedrogomes1.png" />
@@ -23,7 +27,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Fala galeraa 👋</p>
+          <p>{content}</p>
         </div>
 
         <footer>
